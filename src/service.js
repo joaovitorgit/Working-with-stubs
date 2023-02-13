1fc13126c04bae07490e6ea70e90c21f594b548b
Fulimirani <fulimirani@gmail.com>
2023-02-13 07:38:49 -0300

    13-02

---
+++ src/service.js
@@ -0,0 +1,24 @@
+
+const https = require('https')
+
+class Service{
+    async makeRequest(url){
+        return new Promise((resolve, reject)=>{
+            https.get(url, response=>{
+                response.on("data", data=>resolve(JSON.parse(data)))
+                response.on("error", reject)
+            })
+        })
+    }
+    async getPlanets(url){
+        const result = await this.makeRequest(url)
+
+        return {
+            name: result.name,
+            surface_water: result.surface_water,
+            appearedIn: result.films.length
+        }
+    }
+}
+
+module.exports = Service
